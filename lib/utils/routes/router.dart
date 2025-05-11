9ceaf0e054d0211f8b6c562488ca8f3e3d7ab779
Noorey Schaultz <ahmadnaufal588@gmail.com>
2025-05-11 12:49:29 +0700

    add new routes

--- lib/utils/routes/router.dart
+++ lib/utils/routes/router.dart
@@ -1,5 +1,6 @@
 import 'package:braille_app/ui/screen/document/document_screen.dart';
 import 'package:braille_app/ui/screen/home/home_screen.dart';
+import 'package:braille_app/ui/screen/scanner/text_scanner.dart';
 import 'package:flutter/material.dart';
 
 class AppRoute {
@@ -9,6 +10,8 @@ class AppRoute {
         return _materialRoute(const HomeScreen());
       case '/document':
         return _materialRoute(const DocumentScreen());
+      case '/text-scanner':
+        return _materialRoute(const TextScanner());
       default:
         return _materialRoute(const HomeScreen());
     }
