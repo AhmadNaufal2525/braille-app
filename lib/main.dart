5dd9221aeb3bb4f0d6ca02fb7bdfda75841d510b
Noorey Schaultz <ahmadnaufal588@gmail.com>
2025-05-11 11:05:56 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'TemaNetra',
       theme: AppTheme.theme,
       onGenerateRoute: AppRoute.onGenerateRoutes,
     );
