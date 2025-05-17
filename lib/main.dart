9ef8436a3df6ff7d78929b0ef99f3b2eee9d9c00
Noorey Schaultz <ahmadnaufal588@gmail.com>
2025-05-17 16:38:35 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:braille_app/utils/config/theme/app_theme.dart';
 import 'package:braille_app/utils/routes/router.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
