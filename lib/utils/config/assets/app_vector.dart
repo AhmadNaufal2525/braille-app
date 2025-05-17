abeafc5f70434055119f3441569e885a11c0fd4a
Noorey Schaultz <ahmadnaufal588@gmail.com>
2025-05-17 16:37:58 +0700

    add image and vectors

--- lib/utils/config/assets/app_vector.dart
+++ lib/utils/config/assets/app_vector.dart
@@ -1,4 +1,6 @@
 class AppVectors {
   static const String basePath = 'assets/vectors/';
   static const String format = '.svg';
+  static const String onboardShape = '${basePath}onboard_shape$format';
+  static const String authShape = '${basePath}auth_shape$format';
 }
