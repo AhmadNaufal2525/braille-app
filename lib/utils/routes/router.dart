9b9831cb4e9b5f319d803a8ba9fe6fd7052e4d9c
Noorey Schaultz <ahmadnaufal588@gmail.com>
2025-05-21 07:30:25 +0700

    add splash routes

--- lib/utils/routes/router.dart
+++ lib/utils/routes/router.dart
@@ -2,13 +2,17 @@ import 'package:braille_app/ui/screen/document/document_screen.dart';
 import 'package:braille_app/ui/screen/home/home_screen.dart';
 import 'package:braille_app/ui/screen/onboard/onboard_screen.dart';
 import 'package:braille_app/ui/screen/scanner/text_scanner.dart';
+import 'package:braille_app/ui/screen/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 class AppRoute {
   static Route<dynamic>? onGenerateRoutes(RouteSettings settings) {
     switch (settings.name) {
       case '/':
-        return _materialRoute(const OnboardScreen());
+        return _materialRoute(SplashScreen());
+      case '/onboard':
+        final title = settings.arguments as String;
+        return _materialRoute(OnboardScreen(title: title));
       case '/home':
         return _materialRoute(const HomeScreen());
       case '/document':
