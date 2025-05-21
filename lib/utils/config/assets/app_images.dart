a531d3dfadd7acd9c3879a228e869e3f793f1485
Noorey Schaultz <ahmadnaufal588@gmail.com>
2025-05-21 07:30:04 +0700

    convert all png to svg

--- lib/utils/config/assets/app_images.dart
+++
@@ -1,9 +0,0 @@
-class AppImages {
-  static const String basePath = 'assets/images/';
-  static const String format = '.png';
-  static const String onboardImage1 = '${basePath}onboard_1$format';
-  static const String onboardImage2 = '${basePath}onboard_2$format';
-  static const String onboardImage3 = '${basePath}onboard_3$format';
-  static const String brailleIcon = '${basePath}braille$format';
-  static const String homeIcon = '${basePath}home$format';
-}
--- lib/utils/config/assets/app_vector.dart
+++ lib/utils/config/assets/app_vector.dart
@@ -3,4 +3,10 @@ class AppVectors {
   static const String format = '.svg';
   static const String onboardShape = '${basePath}onboard_shape$format';
   static const String authShape = '${basePath}auth_shape$format';
+  static const String onboardImg1 = '${basePath}onboard_1$format';
+  static const String onboardImg2 = '${basePath}onboard_2$format';
+  static const String onboardImg3 = '${basePath}onboard_3$format';
+  static const String iconHome = '${basePath}home$format';
+  static const String iconBraille = '${basePath}braille$format';
+  static const String logo = '${basePath}logo$format';
 }
