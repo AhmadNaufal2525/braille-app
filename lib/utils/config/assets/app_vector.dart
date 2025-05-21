8c82f629abd429d3b9193a9dbde9b961b7dd4e30
Noorey Schaultz <ahmadnaufal588@gmail.com>
2025-05-21 09:05:37 +0700

    delete logo vectors

--- lib/utils/config/assets/app_vector.dart
+++ lib/utils/config/assets/app_vector.dart
@@ -8,5 +8,4 @@ class AppVectors {
   static const String onboardImg3 = '${basePath}onboard_3$format';
   static const String iconHome = '${basePath}home$format';
   static const String iconBraille = '${basePath}braille$format';
-  static const String logo = '${basePath}logo$format';
 }
