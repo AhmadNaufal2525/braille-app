5471cced811a0f04df3398956975c6cbb2d47d4c
Noorey Schaultz <ahmadnaufal588@gmail.com>
2025-05-22 21:33:48 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,32 @@ import 'package:braille_app/utils/config/theme/app_theme.dart';
 import 'package:braille_app/utils/routes/router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
   );
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'TemaNetra',
-      theme: AppTheme.theme,
-      onGenerateRoute: AppRoute.onGenerateRoutes,
+    return ScreenUtilInit(
+      designSize: const Size(360, 800),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'TemaNetra',
+          theme: AppTheme.theme,
+          onGenerateRoute: AppRoute.onGenerateRoutes,
+        );
+      },
     );
   }
 }
