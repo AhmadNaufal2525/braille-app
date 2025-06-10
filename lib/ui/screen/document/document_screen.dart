48c3a382a808b813c79e22eaf8c52739604ae1fd
Noorey Schaultz <ahmadnaufal588@gmail.com>
2025-06-11 06:42:55 +0700

    update document screen

--- lib/ui/screen/document/document_screen.dart
+++ lib/ui/screen/document/document_screen.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:archive/archive_io.dart';
+import 'package:braille_app/utils/config/assets/app_vector.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:syncfusion_flutter_pdf/pdf.dart';
@@ -94,169 +96,216 @@ class _DocumentScreenState extends State<DocumentScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+      body: Column(
+        children: [
+          Stack(
             children: [
-              const Label(text: 'Choose Document'),
-              const SizedBox(height: 12),
-              BasicButton(
-                textStyle: TextStyle(
-                  fontSize: 14.0,
-                  color: AppColors.whiteColor,
-                ),
-                text: 'Select PDF or TXT',
-                onPress: pickFile,
+              SvgPicture.asset(
+                AppVectors.wellcomeVector,
+                fit: BoxFit.cover,
                 width: double.infinity,
-                height: 48,
+                height:
+                    MediaQuery.of(context).size.height < 700 ? 145.h : 130.h,
               ),
-              const SizedBox(height: 18),
-              Expanded(
-                child: DottedBorder(
-                  color: AppColors.blackColor,
-                  strokeWidth: 1,
-                  dashPattern: [6, 3],
-                  borderType: BorderType.RRect,
-                  radius: const Radius.circular(8),
-                  child: Container(
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 40, horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Hi Teman',
+                      style:
+                          MediaQuery.of(context).size.height < 700
+                              ? AppTextStyle.largeWhite
+                              : AppTextStyle.xlargeWhite,
+                    ),
+                    Text(
+                      'Selamat Datang',
+                      style: AppTextStyle.xxlargeWhite.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Label(text: 'Choose Document'),
+                  const SizedBox(height: 12),
+                  BasicButton(
+                    textStyle: TextStyle(
+                      fontSize: 14.0,
+                      color: AppColors.whiteColor,
+                    ),
+                    text: 'Select PDF or TXT',
+                    onPress: pickFile,
                     width: double.infinity,
-                    padding: const EdgeInsets.all(12),
-                    child:
-                        plainTextController.text.isEmpty
-                            ? Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Icon(
-                                  Icons.insert_drive_file_rounded,
-                                  size: 40.sp,
-                                  color: Colors.grey.shade300,
-                                ),
-                                const SizedBox(height: 8),
-                                Text(
-                                  'Upload or drop document to translate',
-                                  style: TextStyle(
-                                    color: Colors.grey.shade500,
-                                    fontSize: 14.sp,
-                                  ),
-                                ),
-                                const SizedBox(height: 4),
-                                Text(
-                                  'Max File Size 10 Mb',
-                                  style: TextStyle(
-                                    color: Colors.grey.shade500,
-                                    fontSize: 14.sp,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ],
-                            )
-                            : SingleChildScrollView(
-                              child: TextField(
-                                controller: plainTextController,
-                                style: AppTextStyle.mediumBlack,
-                                maxLines: null,
-                                decoration: const InputDecoration.collapsed(
-                                  hintText: 'No text extracted.',
-                                ),
-                              ),
-                            ),
+                    height: 48,
                   ),
-                ),
-              ),
-              const SizedBox(height: 8),
-              BasicButton(
-                text: 'Convert to Braille',
-                textStyle: TextStyle(
-                  fontSize: 14.0,
-                  color: AppColors.whiteColor,
-                ),
-                onPress: convertToBraille,
-                width: double.infinity,
-                height: 48,
-              ),
-              const SizedBox(height: 12),
-              const Label(text: 'Braille Text'),
-              Expanded(
-                child: DottedBorder(
-                  color: AppColors.blackColor,
-                  strokeWidth: 1,
-                  dashPattern: [6, 3],
-                  borderType: BorderType.RRect,
-                  radius: const Radius.circular(8),
-                  child: Container(
-                    width: double.infinity,
-                    padding: const EdgeInsets.all(12),
-                    child: SingleChildScrollView(
-                      child: Text(
-                        brailleText.isEmpty
-                            ? 'No braille text yet.'
-                            : brailleText,
-                        style: AppTextStyle.mediumBlack,
+                  SizedBox(height: 18.h),
+                  Expanded(
+                    child: DottedBorder(
+                      color: AppColors.blackColor,
+                      strokeWidth: 1,
+                      dashPattern: [6, 3],
+                      borderType: BorderType.RRect,
+                      radius: const Radius.circular(8),
+                      child: Container(
+                        width: double.infinity,
+                        padding: const EdgeInsets.all(12),
+                        child:
+                            plainTextController.text.isEmpty
+                                ? Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    SvgPicture.asset(
+                                      AppVectors.iconDoc,
+                                      width: 50.w,
+                                      height: 50.h,
+                                      colorFilter: ColorFilter.mode(
+                                        AppColors.hintDocumentColor,
+                                        BlendMode.srcIn,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 8),
+                                    Text(
+                                      'Upload or drop document to translate',
+                                      style: TextStyle(
+                                        color: AppColors.hintDocumentColor,
+                                        fontSize: 10.sp,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 4),
+                                    Text(
+                                      'Max File Size 10 Mb',
+                                      style: TextStyle(
+                                        color: AppColors.hintDocumentColor,
+                                        fontSize: 10.sp,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                )
+                                : SingleChildScrollView(
+                                  child: TextField(
+                                    controller: plainTextController,
+                                    style: AppTextStyle.mediumBlack,
+                                    maxLines: null,
+                                    decoration: const InputDecoration.collapsed(
+                                      hintText: 'No text extracted.',
+                                    ),
+                                  ),
+                                ),
                       ),
                     ),
                   ),
-                ),
-              ),
-              const SizedBox(height: 8),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
+                  SizedBox(height: 12.h),
                   BasicButton(
-                    text: 'Copy',
-                    backgroundColor: AppColors.whiteColor,
-                    width: 56.w,
-                    height:
-                        MediaQuery.of(context).size.height < 700 ? 30.h : 24.h,
-                    border: BorderSide(
-                      color: AppColors.primaryColor,
-                      width: 1.w,
+                    text: 'Convert to Braille',
+                    textStyle: TextStyle(
+                      fontSize: 14.0,
+                      color: AppColors.whiteColor,
                     ),
-                    textStyle: AppTextStyle.smallGreenBold,
-                    onPress: () {
-                      copyToClipboard(brailleText);
-                    },
+                    onPress: convertToBraille,
+                    width: double.infinity,
+                    height: 48,
                   ),
-                  BasicButton(
-                    text: 'Save as doc',
-                    backgroundColor: AppColors.whiteColor,
-                    width: 56.w,
-                    height:
-                        MediaQuery.of(context).size.height < 700 ? 30.h : 24.h,
-                    border: BorderSide(
-                      color: AppColors.primaryColor,
-                      width: 1.w,
+                  const SizedBox(height: 12),
+                  const Label(text: 'Braille Text'),
+                  Expanded(
+                    child: DottedBorder(
+                      color: AppColors.blackColor,
+                      strokeWidth: 1,
+                      dashPattern: [6, 3],
+                      borderType: BorderType.RRect,
+                      radius: const Radius.circular(8),
+                      child: Container(
+                        width: double.infinity,
+                        padding: const EdgeInsets.all(12),
+                        child: SingleChildScrollView(
+                          child: Text(
+                            brailleText.isEmpty
+                                ? 'No braille text yet.'
+                                : brailleText,
+                            style: AppTextStyle.mediumBlack,
+                          ),
+                        ),
+                      ),
                     ),
-                    textStyle: AppTextStyle.smallGreenBold,
-                    onPress: () {
-                      exportAsDocx(brailleText);
-                    },
                   ),
-                  BasicButton(
-                    text: 'Reset',
-                    backgroundColor: AppColors.whiteColor,
-                    width: 56.w,
-                    height:
-                        MediaQuery.of(context).size.height < 700 ? 30.h : 24.h,
-                    border: BorderSide(
-                      color: AppColors.primaryColor,
-                      width: 1.w,
-                    ),
-                    textStyle: AppTextStyle.smallGreenBold,
-                    onPress: () {
-                      setState(() {
-                        brailleText = '';
-                      });
-                      plainTextController.clear();
-                    },
+                  const SizedBox(height: 8),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      BasicButton(
+                        text: 'Copy',
+                        backgroundColor: AppColors.whiteColor,
+                        width: 56.w,
+                        height:
+                            MediaQuery.of(context).size.height < 700
+                                ? 30.h
+                                : 24.h,
+                        border: BorderSide(
+                          color: AppColors.primaryColor,
+                          width: 1.w,
+                        ),
+                        textStyle: AppTextStyle.smallGreenBold,
+                        onPress: () {
+                          copyToClipboard(brailleText);
+                        },
+                      ),
+                      BasicButton(
+                        text: 'Save as doc',
+                        backgroundColor: AppColors.whiteColor,
+                        width: 56.w,
+                        height:
+                            MediaQuery.of(context).size.height < 700
+                                ? 30.h
+                                : 24.h,
+                        border: BorderSide(
+                          color: AppColors.primaryColor,
+                          width: 1.w,
+                        ),
+                        textStyle: AppTextStyle.smallGreenBold,
+                        onPress: () {
+                          exportAsDocx(brailleText);
+                        },
+                      ),
+                      BasicButton(
+                        text: 'Reset',
+                        backgroundColor: AppColors.whiteColor,
+                        width: 56.w,
+                        height:
+                            MediaQuery.of(context).size.height < 700
+                                ? 30.h
+                                : 24.h,
+                        border: BorderSide(
+                          color: AppColors.primaryColor,
+                          width: 1.w,
+                        ),
+                        textStyle: AppTextStyle.smallGreenBold,
+                        onPress: () {
+                          setState(() {
+                            brailleText = '';
+                          });
+                          plainTextController.clear();
+                        },
+                      ),
+                    ],
                   ),
+                  30.verticalSpace,
                 ],
               ),
-              30.verticalSpace,
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
